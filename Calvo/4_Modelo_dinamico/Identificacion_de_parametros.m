8e3ff293c3ba4f36a26334427bb9520e77509921
Alvaro Calvo Matos <alvarocalvomatos@gmail.com>
2019-11-10 17:35:09 +0100

    masodkjfn

--- Calvo/4_Modelo_dinamico/Identificacion_de_parametros.m
+++ Calvo/4_Modelo_dinamico/Identificacion_de_parametros.m
@@ -1,30 +1,5 @@
 %% Obtencion de las matrices M V y G
-clear all;
-close all;
-clc;
-
-% Variables simbolicas necesarias
-syms T1 T2 T3 q1 qd1 qdd1 q2 qd2 qdd2 q3 qd3 qdd3 g real
-syms m1 m2 m3 l1 l2 l3 Ixx1 Ixx2 Ixx3 Iyy1 Iyy2 Iyy3 Izz1 Izz2 Izz3 real
-syms Jm1 Jm2 Jm3 Bm1 Bm2 Bm3 real
-
-gamma_reducida = [ qdd1, 2500*qd1, (49*qdd1)/200 + (49*qdd1*cos(2*q2))/200 - (49*qd1*qd2*sin(2*q2))/100, (7*qd1*qd2*sin(2*q2))/5 - (7*qdd1*cos(2*q2))/10 - (7*qdd1)/10, qd1*qd2*sin(2*q2) - qdd1*(cos(2*q2)/2 - 1/2),       0, (37*qdd1)/100 + (49*qdd1*cos(2*q2))/200 + (qdd1*cos(2*q2 + 2*q3))/8 + (7*qdd1*cos(q3))/20 + (7*qdd1*cos(2*q2 + q3))/20 - (7*qd1*qd3*sin(q3))/20 - (7*qd1*qd2*sin(2*q2 + q3))/10 - (7*qd1*qd3*sin(2*q2 + q3))/20 - (49*qd1*qd2*sin(2*q2))/100 - (qd1*qd2*sin(2*q2 + 2*q3))/4 - (qd1*qd3*sin(2*q2 + 2*q3))/4, (7*qd1*qd3*sin(q3))/10 - (qdd1*cos(2*q2 + 2*q3))/2 - (7*qdd1*cos(q3))/10 - (7*qdd1*cos(2*q2 + q3))/10 - qdd1/2 + (7*qd1*qd2*sin(2*q2 + q3))/5 + (7*qd1*qd3*sin(2*q2 + q3))/10 + qd1*qd2*sin(2*q2 + 2*q3) + qd1*qd3*sin(2*q2 + 2*q3), (qd1*(100*qd2*sin(2*q2 + 2*q3) + 100*qd3*sin(2*q2 + 2*q3)))/100 - qdd1*(cos(2*q2 + 2*q3)/2 - 1/2),        0,       0;
-                      0,        0,          (49*sin(2*q2)*qd1^2)/200 + (49*qdd2)/100 + (7*g*cos(q2))/10,             - (7*qdd2)/5 - (7*qd1^2*sin(2*q2))/10 - g*cos(q2),                         -(qd1^2*sin(2*q2))/2, 900*qd2,                                                            (37*qdd2)/50 + qdd3/4 - (7*qd3^2*sin(q3))/20 + (7*qd1^2*sin(2*q2 + q3))/20 + (g*cos(q2 + q3))/2 + (49*qd1^2*sin(2*q2))/200 + (7*g*cos(q2))/10 + (qd1^2*sin(2*q2 + 2*q3))/8 + (7*qdd2*cos(q3))/10 + (7*qdd3*cos(q3))/20 - (7*qd2*qd3*sin(q3))/10,                                                   (7*sin(q3)*qd3^2)/10 + (7*qd2*sin(q3)*qd3)/5 - qdd2 - qdd3 - (7*qd1^2*sin(2*q2 + q3))/10 - g*cos(q2 + q3) - (qd1^2*sin(2*q2 + 2*q3))/2 - (7*qdd2*cos(q3))/5 - (7*qdd3*cos(q3))/10,                                                                       -(qd1^2*sin(2*q2 + 2*q3))/2,        0,       0;
-                      0,        0,                                                                    0,                                                             0,                                            0,       0,                                                                                                                                        qdd2/4 + qdd3/4 + (7*qd1^2*sin(q3))/40 + (7*qd2^2*sin(q3))/20 + (7*qd1^2*sin(2*q2 + q3))/40 + (g*cos(q2 + q3))/2 + (qd1^2*sin(2*q2 + 2*q3))/8 + (7*qdd2*cos(q3))/20,                                                                       - qdd2 - qdd3 - (7*qd1^2*sin(q3))/20 - (7*qd2^2*sin(q3))/10 - (7*qd1^2*sin(2*q2 + q3))/20 - g*cos(q2 + q3) - (qd1^2*sin(2*q2 + 2*q3))/2 - (7*qdd2*cos(q3))/10,                                                                       -(qd1^2*sin(2*q2 + 2*q3))/2, 225*qdd3, 225*qd3];
- 
-
-thita_reducida = [    Iyy1 + Iyy2 + Iyy3 - Izz2 - Izz3 + 2500*Jm1 - 900*Jm2
-                                                                        Bm1
-4*m3*l3^2 - (100*Izz2)/49 + 4*Izz3 - (90000*Jm2)/49 + m2 - (100*l2^2*m2)/49
-                        l2*m2 - (9000*Jm2)/7 - (10*Izz2)/7 - (10*l2^2*m2)/7
-                                               Ixx2 - Iyy2 + Izz2 + 900*Jm2
-                                                                        Bm2
-                                                  - 4*m3*l3^2 - 4*Izz3 + m3
-                                               - 2*m3*l3^2 + m3*l3 - 2*Izz3
-                                                         Ixx3 - Iyy3 + Izz3
-                                                                        Jm3 
-                                                                        Bm3];
-%% Estimacion de parámetros
+% Estimacion de parámetros
 % Accionamiento directo (Parametros con medidas perfectas)
 clear all;
 syms q1 qd1 qdd1 q2 qd2 qdd2 q3 qd3 qdd3 g real
@@ -59,6 +34,66 @@ thita_reducida = [
         0.0149999999999998
           2.91314677499997
          0.971048924999985
+];
+
+Im = simplify(gamma_reducida*thita_reducida);
+
+qdd = [qdd1, qdd2, qdd3]';
+qd = [qd1, qd2, qd3]';
+
+M = sym(zeros(3,3));
+V = sym(zeros(3,1));
+G = sym(zeros(3,1));
+
+for i = 1:3
+    for j = 1:3
+        M(i,j) = diff(Im(i), qdd(j));
+    end
+    G(i) = diff(Im(i), g)*g;
+    V(i) = Im(i) - M(i,:)*qdd - G(i);
+end
+
+% R, Jm y Bm ya estan inluidas en M V y G
+Ma = simplify(M);
+Va = simplify(V);
+Ga = simplify(G);
+
+%% Obtencion de las matrices M V y G
+% Estimacion de parámetros
+% Accionamiento directo (Parametros con medidas ruidosas)
+clear all;
+syms q1 qd1 qdd1 q2 qd2 qdd2 q3 qd3 qdd3 g real
+
+l0 = 1.00;
+l1 = 0.40;
+l2 = 0.70;
+l3 = 0.50;
+
+R1 = 1;
+R2 = 1;
+R3 = 1;
+
+Kt1=0.5;
+Kt2=0.4;
+Kt3=0.35;
+
+gamma_reducida = [
+    (1/(Kt1*R1))*[ qdd1/2 - (qdd1*cos(2*q2))/2 + qd1*qd2*sin(2*q2), qdd1/2 - (qdd1*cos(2*q2 + 2*q3))/2 + qd1*qd2*sin(2*q2 + 2*q3) + qd1*qd3*sin(2*q2 + 2*q3), qdd1,    0,           0,         0, R1^2*qd1,        0,        0,         0,           -l2*(qd1*qd3*sin(q3) - qdd1*cos(2*q2 + q3) - qdd1*cos(q3) + 2*qd1*qd2*sin(2*q2 + q3) + qd1*qd3*sin(2*q2 + q3))];
+    (1/(Kt2*R2))*[                            -(qd1^2*sin(2*q2))/2,                                                              -(qd1^2*sin(2*q2 + 2*q3))/2,    0, qdd2, qdd2 + qdd3,         0,        0, R2^2*qd2,        0, g*cos(q2), l2*sin(2*q2 + q3)*qd1^2 - l2*sin(q3)*qd3^2 - 2*l2*qd2*sin(q3)*qd3 + g*cos(q2 + q3) + 2*l2*qdd2*cos(q3) + l2*qdd3*cos(q3)];
+    (1/(Kt3*R3))*[                                               0,                                                              -(qd1^2*sin(2*q2 + 2*q3))/2,    0,    0, qdd2 + qdd3, R3^2*qdd3,        0,        0, R3^2*qd3,         0,                 g*cos(q2 + q3) + l2*qdd2*cos(q3) + (l2*qd1^2*sin(q3))/2 + l2*qd2^2*sin(q3) + (l2*qd1^2*sin(2*q2 + q3))/2]];
+    
+thita_reducida = [
+                     0
+                     0
+                     0
+                     0
+                     0
+                     0
+                     0
+                     0
+                     0
+                     0
+                     0
 ];
 
 Im = simplify(gamma_reducida*thita_reducida);
--- Calvo/4_Modelo_dinamico/ModeloDinamico_R3GDL.m
+++ Calvo/4_Modelo_dinamico/ModeloDinamico_R3GDL.m
@@ -15,6 +15,7 @@ Im3      = in(9);
 
 % Parámetros base estimados:
 t_red=[
+%  Modelo perfecto con reductoras
 %    -1.7594
 %    -0.3966
 %     8.3284
@@ -26,37 +27,33 @@ t_red=[
 %     0.0150
 %     2.9131
 %     0.9710
-<<<<<<< HEAD
-   -8.4807
-   -2.3902
-   11.1254
-    5.6368
-   2.1748
-    0.0139
-    0.0113
-   0.0032
-    0.0133
-    1.2074
-    0.3639
 
-=======
+% Modelo ruidoso con reductoras
+    -1.7507
+    -0.3874
+     8.2151
+     15.973
+     0.3841
+     0.0080
+     0.0114
+     0.0069
+     0.0150
+     2.9975
+     0.9856
 
-
-         -1.75940038424998
-        -0.396642654749998
-          2.16551925299997
-          1.77669585024997
-         0.397667654749993
-       0.00722506600000024
-        0.0119999999999998
-       0.00849999999999985
-        0.0149999999999998
-          2.91314677499997
-         0.971048924999985
->>>>>>> 3623cc8a2dde2ec97e74a804993e7b262874012a
+%  Modelo perfecto con accionamiento directo
+%          -1.75940038424998
+%         -0.396642654749998
+%           2.16551925299997
+%           1.77669585024997
+%          0.397667654749993
+%        0.00722506600000024
+%         0.0119999999999998
+%        0.00849999999999985
+%         0.0149999999999998
+%           2.91314677499997
+%          0.971048924999985
 ];
-%                    probar load/save theta 
-
 
 
 % Datos geométricos / gravedad / reductoras / constantes de par
@@ -65,57 +62,51 @@ l1 = 0.40;
 l2 = 0.70;
 l3 = 0.50;
 
-g=9.81;
+g = 9.81;
 
-R1=1;
-R2=1;
-R3=1;
+R1 = 1;
+R2 = 1;
+R3 = 1;
 
-% R1=50;
-% R2=30;
-% R3=15;
+% R1 = 50;
+% R2 = 30;
+% R3 = 15;
 
-R=diag([R1 R2 R3]);
+R = diag([R1 R2 R3]);
 
-Kt1=0.5;
-Kt2=0.4;
-Kt3=0.35;
-Kt=diag([Kt1 Kt2 Kt3]);
+Kt1 = 0.5;
+Kt2 = 0.4;
+Kt3 = 0.35;
+Kt = diag([Kt1 Kt2 Kt3]);
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Modelo en forma matricial (M, V, G) (solucion de Raul)
+M11 = [t_red(3)+t_red(11)*l2*(cos(2*q2+q3)+cos(q3))+1/2*(t_red(1)+t_red(2)-t_red(1)*cos(2*q2)-t_red(2)*cos(2*q2+2*q3))];
+M12 = 0;
+M13 = 0;
+M21 = M12;
+M22 = [t_red(4)+t_red(5)+2*l2*t_red(11)*cos(q3)];
+M23 = [t_red(5)+l2*t_red(11)*cos(q3)];
+M31 = M13;
+M32 = M23;
+M33 = [t_red(5)+R3^2*t_red(6)];
 
-% Modelo en forma matricial (M, V, G)
-M11=[t_red(3)+t_red(11)*l2*(cos(2*q2+q3)+cos(q3))+1/2*(t_red(1)+t_red(2)-t_red(1)*cos(2*q2)-t_red(2)*cos(2*q2+2*q3))];
-M12=0;
-M13=0;
-M21=M12;
-M22=[t_red(4)+t_red(5)+2*l2*t_red(11)*cos(q3)];
-M23=[t_red(5)+l2*t_red(11)*cos(q3)];
-M31=M13;
-M32=M23;
-M33=[t_red(5)+R3^2*t_red(6)];
+M = [M11 M12 M13
+     M21 M22 M23
+     M31 M32 M33];
 
-M=[M11 M12 M13
-    M21 M22 M23
-    M31 M32 M33];
+V1 = [qd1*( t_red(7)*R1^2 +qd2*(sin(2*q2)*t_red(1) +sin(2*q2+2*q3)*t_red(2) - 2*l2*t_red(11)*sin(2*q2+q3)) + qd3*(sin(2*q2+2*q3)*t_red(2) -l2*t_red(11)*sin(2*q2+q3) -l2*t_red(11)*sin(q3) )     ) ];
+V2 = ( (-1/2*qd1^2*(sin(2*q2)*t_red(1) + sin(2*q2+2*q3)*t_red(2) - 2*l2*t_red(11)*sin(2*q2+q3) ))  + (qd2*t_red(8)*R2^2) - l2*t_red(11)*sin(q3)*qd3^2 - 2*l2*t_red(11)*qd2*sin(q3)*qd3 );
+V3 = (    (qd3*R3^2*t_red(9)) + (qd2^2*sin(q3)*l2*t_red(11))  + qd1^2*(-t_red(2)*sin(2*q2 + 2*q3) +l2*t_red(11)*(sin(2*q2 + q3) + sin(q3))   )/2  );
 
-V1=[qd1*( t_red(7)*R1^2 +qd2*(sin(2*q2)*t_red(1) +sin(2*q2+2*q3)*t_red(2) - 2*l2*t_red(11)*sin(2*q2+q3)) + qd3*(sin(2*q2+2*q3)*t_red(2) -l2*t_red(11)*sin(2*q2+q3) -l2*t_red(11)*sin(q3) )     ) ];
-V2=( (-1/2*qd1^2*(sin(2*q2)*t_red(1) + sin(2*q2+2*q3)*t_red(2) - 2*l2*t_red(11)*sin(2*q2+q3) ))  + (qd2*t_red(8)*R2^2) - l2*t_red(11)*sin(q3)*qd3^2 - 2*l2*t_red(11)*qd2*sin(q3)*qd3 );
-V3=(    (qd3*R3^2*t_red(9)) + (qd2^2*sin(q3)*l2*t_red(11))  + qd1^2*(-t_red(2)*sin(2*q2 + 2*q3) +l2*t_red(11)*(sin(2*q2 + q3) + sin(q3))   )/2  );
+V = [V1 V2 V3]';
 
-V=[V1 V2 V3]';
-
-G1=0;
-G2=g*(t_red(10)*cos(q2) +t_red(11)*cos(q2+q3));
-G3=g*t_red(11)*cos(q2+q3);
+G1 = 0;
+G2 = g*(t_red(10)*cos(q2) +t_red(11)*cos(q2+q3));
+G3 = g*t_red(11)*cos(q2+q3);
 
 G=[G1 G2 G3]';
-
-% Comprobar que M es definida postiva !!!!!
-
-
-%   %   
+  
 Im=[Im1 Im2 Im3]';
 
 qdd = inv(M)*(Kt*R*Im-V-G);
